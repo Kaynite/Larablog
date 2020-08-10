6da584be8d72c20b08f08a90aad262dd98496d35
Kareem <kudo.200200@gmail.com>
2020-08-10 19:39:46 +0200

    added views column

--- app/Post.php
+++ app/Post.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillable = ["title", "body", "category_id", "author", "image"];
+    protected $fillable = ["title", "body", "category_id", "author", "image", "views"];
 
     public function category() {
         return $this->belongsTo("App\Category");
